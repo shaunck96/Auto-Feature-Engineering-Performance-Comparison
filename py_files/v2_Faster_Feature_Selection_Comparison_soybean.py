5cdf964e545fa17633c5c04d16a2ee560862dc1c
shaunck96 <79271767+shaunck96@users.noreply.github.com>
2022-02-05 10:35:20 -0500

    Add files via upload

---
+++ py_files/v2_Faster_Feature_Selection_Comparison_soybean.py
@@ -0,0 +1,565 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Here is a simpler process
+
+# In[1]:
+
+
+from platform import python_version
+import autokeras as ak
+import datetime as dt
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+from sklearn.metrics import f1_score
+print(python_version())
+import scipy
+print('scipy:'+scipy.__version__)
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from sklearn.metrics import log_loss
+import time
+
+
+# In[2]:
+
+
+import os
+
+os.getcwd()
+
+
+# # Read the file
+
+# In[3]:
+
+
+datapath = r"C:\Users\shaun\OneDrive\Desktop\Feature_Engineering_Project\soybean.csv"
+#datapath = './'
+#filename = 'ionosphere.csv'
+df = pd.read_csv(datapath)
+print(df.shape)
+df.head()
+
+
+# In[4]:
+
+
+target = 'Target'
+
+
+# In[5]:
+
+
+#Extract list of numeric columns
+cols = df.select_dtypes([np.number]).columns
+cols = list(cols)
+#cols.remove('Target')
+
+
+# In[6]:
+
+
+df[target].hist()
+
+
+# In[7]:
+
+
+from sklearn.preprocessing import LabelEncoder
+lb_make = LabelEncoder()
+df[target] = lb_make.fit_transform(df[target])
+df[[target]].head(1)
+
+
+# In[8]:
+
+
+df = df.dropna()
+
+
+# Importing PyImpetus, Eli5, EvolutionaryFS, autofeat ; To Include More from https://www.opensourceagenda.com/tags/feature-selection?page=2
+
+# In[9]:
+
+
+from PyImpetus import PPIMBC
+#import eli5
+#from eli5.sklearn import PermutationImportance
+from sklearn.svm import SVC
+from sklearn.feature_selection import SelectFromModel
+from EvolutionaryFS import GeneticAlgorithmFS
+from zoofs import ParticleSwarmOptimization
+
+import featurewiz as fwiz
+from featurewiz import FeatureWiz
+
+from autofeat import FeatureSelector
+
+#from autofeat import AutoFeatRegressor
+
+
+# In[10]:
+
+
+import featurewiz as fwiz
+
+
+# In[11]:
+
+
+preds = [x for x in list(df) if x not in [target]]
+len(preds)
+
+
+# In[12]:
+
+
+X = df[preds]
+y = df[target]
+print(X.shape, y.shape)
+
+
+# In[13]:
+
+
+##get train and test data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1,
+                                                    stratify=y)
+print(X_train.shape, X_test.shape)
+
+
+# In[14]:
+
+
+X_train
+
+
+# In[15]:
+
+
+#Find categorical columns and object columns to transform into numeric: 
+cat_columns = list(X.select_dtypes(['category']).columns)
+obj_columns = list(X.select_dtypes(['object']).columns)
+len(cat_columns), len(obj_columns)
+
+
+# In[16]:
+
+
+#Category+Object Transforms -> You must always fit_transform train while only doing transform for test
+if len(cat_columns+obj_columns):
+    for each_cat in cat_columns+obj_columns:
+        LB = LabelEncoder()
+        X_train[each_cat] = LB.fit_transform(X_train[each_cat])
+        X_test[each_cat] = LB.transform(X_test[each_cat])
+        print(each_cat+' completed transform')
+
+
+# In[17]:
+
+
+#MinMax Scale Features -> You must always fit_transform train while only doing transform for test
+for each_col in cols:
+    scaler = MinMaxScaler(feature_range = (1,100))
+    X_train[each_col] = scaler.fit_transform(X_train[each_col].values.reshape(-1,1))
+    X_test[each_col] = scaler.transform(X_test[each_col].values.reshape(-1,1))
+print('completed Min Max Scaling')
+
+
+# In[18]:
+
+
+X_train.head()
+
+
+# In[19]:
+
+
+train = pd.concat([X_train,y_train],axis=1)
+print(train.shape)
+
+
+# In[20]:
+
+
+test = pd.concat([X_test,y_test],axis=1)
+print(test.shape)
+
+
+# In[21]:
+
+
+time_to_fit = pd.DataFrame(columns=['Feature_Engineering_Technique','Start_Time','End_Time','Time_to_fit'])
+time_to_fit.head()
+
+
+# In[22]:
+
+
+###################################################FeatureWiz#############################################################
+### Now run it through featurewiz - remember featurewiz does not look at test data only modifies for numeric
+start_featurewiz = time.time()
+
+features = FeatureWiz(corr_limit=0.70, verbose=2, sep=',', header=0, feature_engg='', category_encoders='',dask_xgboost_flag=False, nrows=None)
+X_train_selected = features.fit_transform(X_train,y_train)
+end_featurewiz = time.time()
+diff = end_featurewiz - start_featurewiz
+best_columns_featurewiz = features.features
+len(best_columns_featurewiz)
+time_to_fit = time_to_fit.append({'Feature_Engineering_Technique':'FeatureWiz','Start_Time':start_featurewiz,'End_Time':end_featurewiz,'Time_to_fit':diff} , ignore_index=True)
+time_to_fit.head()
+
+
+# In[23]:
+
+
+#################Autofeat#########################
+
+start_autofeat = time.time()
+fsel = FeatureSelector(verbose=1)
+new_X = fsel.fit_transform(X_train, y_train)
+end_autofeat = time.time()
+diff = end_autofeat - start_autofeat
+best_columns_autofeat = list(new_X.columns)
+time_to_fit = time_to_fit.append({'Feature_Engineering_Technique':'Autofeat','Start_Time':start_autofeat,'End_Time':end_autofeat,'Time_to_fit':diff} , ignore_index=True)
+time_to_fit.head()
+
+
+# In[24]:
+
+
+##############FeatureEngine############
+from sklearn.pipeline import Pipeline
+from feature_engine.selection import (
+    DropDuplicateFeatures,
+    DropConstantFeatures,
+    DropDuplicateFeatures,
+    DropCorrelatedFeatures,
+    SmartCorrelatedSelection,
+    SelectByShuffling,
+    SelectBySingleFeaturePerformance,
+    RecursiveFeatureElimination,
+)
+
+pipe = Pipeline([
+    ('constant', DropConstantFeatures(tol=0.998)), # drops constand and quasi-constant altogether
+    ('duplicated', DropDuplicateFeatures()), # drops duplicates
+    ('correlation', SmartCorrelatedSelection( # drops correlated
+        threshold=0.8,
+        selection_method="model_performance",
+        estimator=RandomForestClassifier(n_estimators=10, random_state=1),
+    )),
+])
+
+# find features to remove
+
+start_fe = time.time()
+pipe.fit(X_train, y_train)
+end_fe = time.time()
+
+diff = end_fe - start_fe
+
+print('Number of original variables: ', X_train.shape[1])
+
+X_train_fe = pipe.transform(X_train)
+
+print('Number of variables after selection: ', X_train_fe.shape[1])
+
+best_columns_fe = list(X_train_fe.columns)
+time_to_fit = time_to_fit.append({'Feature_Engineering_Technique':'FeatureEngine','Start_Time':start_fe,'End_Time':end_fe,'Time_to_fit':diff} , ignore_index=True)
+time_to_fit.head()
+
+
+# In[25]:
+
+
+####################################################PyImpetus#####################################################
+start_pyimpetus = time.time()
+model = PPIMBC(LogisticRegression(random_state=27, max_iter=1000, class_weight="balanced"), cv=0, num_simul=20, simul_type=0, simul_size=0.2, sig_test_type="non-parametric", random_state=27, verbose=2, p_val_thresh=0.05)
+# Fit this above object on the train part and transform the train dataset into selected feature subset
+# NOTE: x_train has to be a dataframe and y_train has to be a numpy array
+x_train = model.fit_transform(X_train, y_train.values)
+# Transform the test set as well
+# Check out the scores of each feature. The scores are in order of the selected feature list
+# NOTE: You can use these scores ina feature selection ensemble
+print("Feature importance: ", model.feat_imp_scores)
+# Plot the feature importance scores
+model.feature_importance()
+best_columns_pyimpetus = list(x_train.columns)
+end_pyimpetus = time.time()
+diff = end_pyimpetus - start_pyimpetus
+len(x_train.columns)
+# Convert the data into numpy arrays
+#x_train, x_test = x_train.values, x_test.values
+time_to_fit = time_to_fit.append({'Feature_Engineering_Technique':'PyImpetus','Start_Time':start_pyimpetus,'End_Time':end_pyimpetus,'Time_to_fit':diff} , ignore_index=True)
+time_to_fit.head()
+
+
+# #######################################################Eli5##############################################################
+# 
+# sel = SelectFromModel(
+#     PermutationImportance(SVC(), cv=5),
+#     threshold=0.05,
+# ).fit(X_train, y_train)
+# x_train = sel.transform(X_train)
+# best_columns_eli5 = x_train.columns
+# 
+# TypeError: check_cv() takes from 0 to 2 positional arguments but 3 were given
+
+# Time To Generate Features 
+
+# In[26]:
+
+
+time_to_fit.head()
+
+
+# In[27]:
+
+
+#############################################EvolutionaryFS#############################################
+##get train and test data
+data_dict={0:{'x_train':X_train,'y_train':y_train,'x_test':X_test,'y_test':y_test}}
+columns_list=list(X.columns)
+
+start_evolutionaryfs = time.time()
+model_object=LogisticRegression(n_jobs=-1,random_state=1)
+evoObj=GeneticAlgorithmFS(model=model_object,data_dict=data_dict,cost_function=f1_score,average='micro',cost_function_improvement='increase',columns_list=columns_list,generations=20,population=30,prob_crossover=0.9,prob_mutation=0.1,run_time=60000)
+best_columns_EvolutionaryFS=evoObj.GetBestFeatures()
+end_evolutionaryfs = time.time()
+diff = end_evolutionaryfs - start_evolutionaryfs
+len(best_columns_EvolutionaryFS)
+time_to_fit = time_to_fit.append({'Feature_Engineering_Technique':'EvolutionaryFS','Start_Time':start_evolutionaryfs,'End_Time':end_evolutionaryfs,'Time_to_fit': diff} , ignore_index=True)
+time_to_fit.head()
+
+
+# In[28]:
+
+
+#########################################################Zoof##############################################
+# define your own objective function, make sure the function receives four parameters,
+#  fit your model and return the objective value ! 
+def objective_function_topass(model,X_train, y_train, X_test, y_test):      
+    model.fit(X_train,y_train)  
+    P=log_loss(y_test,model.predict_proba(X_test))
+    return P
+    
+# create object of algorithm
+algo_object=ParticleSwarmOptimization(objective_function_topass,n_iteration=25,
+                                       population_size=20,minimize=True)
+import lightgbm as lgb
+lgb_model = lgb.LGBMClassifier()                                       
+# fit the algorithm
+start_zoof = time.time()
+
+best_columns_zoof = algo_object.fit(lgb_model,X_train, y_train, X_train, y_train,verbose=True)
+
+end_zoof = time.time()
+diff = end_evolutionaryfs - start_evolutionaryfs
+
+time_to_fit = time_to_fit.append({'Feature_Engineering_Technique':'Zoof','Start_Time':start_zoof,'End_Time':end_zoof,'Time_to_fit': diff} , ignore_index=True)
+
+
+# In[29]:
+
+
+###############################Sklearn Genetic Opt##########################################################
+
+import matplotlib.pyplot as plt
+from sklearn_genetic import GAFeatureSelectionCV
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.svm import SVC
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+clf = SVC(gamma='auto')
+
+evolved_estimator = GAFeatureSelectionCV(
+    estimator=clf,
+    scoring="balanced_accuracy",
+    population_size=30,
+    generations=20,
+    n_jobs=-1)
+
+start_gopt = time.time()
+# Train and select the features
+evolved_estimator.fit(X_train, y_train)
+end_gopt = time.time()
+
+diff = end_gopt-start_gopt
+
+# Features selected by the algorithm
+best_columns_genetic_opt = list(evolved_estimator.best_features_)
+best_columns_genetic_opt = X_train.columns[best_columns_genetic_opt]
+
+time_to_fit = time_to_fit.append({'Feature_Engineering_Technique':'Genetic OPT','Start_Time':start_gopt,'End_Time':end_gopt,'Time_to_fit': diff} , ignore_index=True)
+
+
+# In[30]:
+
+
+##################BorutaPy###################
+from boruta import BorutaPy
+
+# define random forest classifier, with utilising all cores and
+# sampling in proportion to y labels
+rf = RandomForestClassifier(n_jobs=-1, class_weight='balanced', max_depth=5)
+
+# define Boruta feature selection method
+feat_selector = BorutaPy(rf, n_estimators='auto', verbose=2, random_state=1)
+
+start_bpy = time.time()
+feat_selector.fit(X_train.values, y_train)
+end_bpy = time.time()
+
+diff = end_bpy-start_bpy
+
+# check selected features - first 5 features are selected
+feat_selector.support_
+
+# check ranking of features
+feat_selector.ranking_
+
+best_columns_borutapy = list(X_train.loc[:,feat_selector.support_].columns)
+
+time_to_fit = time_to_fit.append({'Feature_Engineering_Technique':'BorutaPy','Start_Time':start_bpy,'End_Time':end_bpy,'Time_to_fit': diff} , ignore_index=True)
+
+
+# In[31]:
+
+
+#################BorutaShap##################
+
+from BorutaShap import BorutaShap
+
+# define random forest classifier, with utilising all cores and
+# sampling in proportion to y labels
+Feature_Selector = BorutaShap(importance_measure='shap',
+                              classification=False)
+
+start_bshap = time.time()
+Feature_Selector.fit(X=X_train, y=y_train, n_trials=100, sample=False,train_or_test = 'test', normalize=True,verbose=True)
+end_bshap = time.time()
+
+diff = end_bshap-start_bshap
+
+# check selected features - first 5 features are selected
+subset = Feature_Selector.Subset()
+
+best_columns_borutashap = list(subset.columns)
+
+time_to_fit = time_to_fit.append({'Feature_Engineering_Technique':'BorutaShap','Start_Time':start_bshap,'End_Time':end_bshap,'Time_to_fit': diff} , ignore_index=True)
+
+
+# In[32]:
+
+
+#Create a dictionary of lists with feature engineering technique
+fs = {"FeatureWiz":best_columns_featurewiz,"Autofeat":best_columns_autofeat,"FeatureEngine":best_columns_fe,"PyImpetus":best_columns_pyimpetus,"EvolutionaryFS":best_columns_EvolutionaryFS,"Zoof":best_columns_zoof, "Genetic OPT":best_columns_genetic_opt, "BorutaPy":best_columns_borutapy, "BorutaShap":best_columns_borutashap}
+#fs
+
+
+# In[33]:
+
+
+## You must set the randomstate for cross validation. Otherwise, scores will be totally random each time.
+from sklearn.model_selection import StratifiedKFold, KFold
+cv = StratifiedKFold(n_splits=5,shuffle=True, random_state=99)
+#cv = KFold(n_splits=5,shuffle=False, random_state=99) ## Use this for Regression
+
+
+# In[34]:
+
+
+#Category+Object Transforms -> You must always fit_transform train while only doing transform for test
+if len(cat_columns+obj_columns):
+    for each_cat in cat_columns+obj_columns:
+        LB = LabelEncoder()
+        X[each_cat] = LB.fit_transform(X[each_cat])
+        print(each_cat+' completed X transform')
+X.head()
+
+
+# In[35]:
+
+
+#MinMax Scale Features -> You must always fit_transform train while only doing transform for test
+for each_col in cols:
+    scaler = MinMaxScaler(feature_range = (1,100))
+    X[each_col] = scaler.fit_transform(X[each_col].values.reshape(-1,1))
+print('completed Min Max Scaling for X')
+
+
+# # Using all features in dataset, this is the baseline score
+
+# In[36]:
+
+
+model1 = RandomForestClassifier(n_estimators=300, random_state=99)
+from sklearn.model_selection import cross_val_score
+scores = cross_val_score(estimator=model1, X=X, y=y, scoring='balanced_accuracy', cv=cv)
+print(scores)
+print('Mean Score for all features = %s' %scores.mean())
+
+
+# ##  Here we run Cross Validation and get accuracy metrics for each technique
+
+# In[37]:
+
+
+performance = pd.DataFrame(columns=['Start_Time','End_Time','Run_Time','Accuracy_Score','Model','Feature_Engineering_Technique'])
+cv = StratifiedKFold(n_splits=5,shuffle=True, random_state=99)
+for key,value in fs.items():
+    start = time.time()
+    model2 = RandomForestClassifier(n_estimators=300, random_state=99)
+    from sklearn.model_selection import cross_val_score
+    scores = cross_val_score(estimator=model2, X=X_train[value], y=y_train, scoring='balanced_accuracy', cv=cv, n_jobs=-1)
+    end = time.time()
+    diff = (end-start)
+    print(scores)
+    print('Mean Score for %s = %s' %(key, scores.mean()))
+    performance = performance.append({'Start_Time':start,'End_Time':end,'Run_Time':diff,'Accuracy_Score':scores.mean(),'Model':'RandomForest','Feature_Engineering_Technique':key} , ignore_index=True)
+    time_to_fit.loc[(time_to_fit['Feature_Engineering_Technique']==key), 'Mean_CV_Accuracy'] = scores.mean()
+time_to_fit.head()
+
+
+# In[38]:
+
+
+#Using Lime to interpret the results of model
+#from lime import lime_text
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import balanced_accuracy_score, mean_squared_error
+#import lime.lime_tabular
+
+for key,value in fs.items():
+    model3 = RandomForestClassifier(n_estimators=300, random_state=99)
+    from sklearn.metrics import classification_report
+    model3.fit(X_train[value], y_train)
+    print(classification_report(y_test, model3.predict(X_test[value])))
+    score_val = balanced_accuracy_score(y_test, model3.predict(X_test[value]))
+    time_to_fit.loc[(time_to_fit['Feature_Engineering_Technique']==key), 'Mean_Test_Accuracy'] = score_val
+    #print("Lime Feature Importance : "+key)
+    #explainer = lime.lime_tabular.LimeTabularExplainer(X_train[value].values, feature_names=X_train[value].columns, class_names=[0, 1], mode='classification')
+    #exp = explainer.explain_instance(X_test[value].values[1], model3.predict_proba, num_features=len(X_train[value].columns))
+    #exp.show_in_notebook(show_table=True, show_all=False)
+
+
+# In[39]:
+
+
+time_to_fit['Dataset'] = "Soybean"
+time_to_fit['Type'] = "Multi Classification"
+time_to_fit['Metric'] = "balanced accuracy"
+
+
+# In[40]:
+
+
+time_to_fit
+
